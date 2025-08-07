33588bc972660ba2974dd0906bdfc62c996c927b
Obaid Malik <obaidmalik@Mehreens-MacBook-Air.local>
2025-08-07 23:09:28 +0100

    Clean up tracked files and update according to .gitignore

--- core_sim/scheduler.py
+++
@@ -1,8 +0,0 @@
-class Scheduler:
-    def __init__(self, agents):
-        self.agents = agents
-
-    def step(self, env):
-        for agent in self.agents:
-            if agent.status == "active":
-                agent.step(env)
